91d35195abf8f11fa26692c567a96e162806378a
工藤 <18883840501@163.com>
2018-10-18 18:07:12 +0800

    no message

---
+++ app/src/main/java/cc/shinichi/exifeditor/Util.kt
@@ -0,0 +1,15 @@
+package cc.shinichi.exifeditor
+
+/**
+ * @author 工藤
+ * @email gougou@16fan.com
+ * cc.shinichi.exifeditor
+ * create at 2018/10/18  18:02
+ * description:
+ */
+object Util {
+
+    fun setExifByTag(imagePath: String, tag: String, valueL: String) {
+
+    }
+}
